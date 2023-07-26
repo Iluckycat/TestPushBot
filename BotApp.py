d07e421fdd30bad86351194de447465d15b1526b
iluckycat <andriei.voronin.2000@gmail.com>
2023-07-26 23:50:16 +0300

    fix echo bot

--- BotApp.py
+++ BotApp.py
@@ -1,12 +1,17 @@
 from bot.bot import Bot
 from bot.handler import MessageHandler
 
-TOKEN = "001.2687836354.0305942298:101090619" #your token here
-
+TOKEN = "001.2687836354.0305942298:1010906191" #your token here
+CHAT = "689061347@chat.agent"
 bot = Bot(token=TOKEN)
+print("I'm ready!")
+
 
 def message_cb(bot, event):
+    print(event)
     bot.send_text(chat_id=event.from_chat, text=event.text)
+
+
 
 bot.dispatcher.add_handler(MessageHandler(callback=message_cb))
 bot.start_polling()
